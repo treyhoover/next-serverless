7eb40c24d649d1ba26ced2a9f72d700353db97b0
Trey Hoover <treyhoover@gmail.com>
2019-02-17 12:53:36 -0500

    Add styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
+import Title from '../components/Title';
 
 export default () => (
-  <ul>
-    <li>
-      <Link href="/a" as="/a">
-        <a>a</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/b" as="/b">
-        <a>b</a>
-      </Link>
-    </li>
-  </ul>
+  <>
+    <Title>Hello</Title>
+
+    <ul>
+      <li>
+        <Link href="/a" as="/a">
+          <a>a</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/b" as="/b">
+          <a>b</a>
+        </Link>
+      </li>
+    </ul>
+  </>
 );
